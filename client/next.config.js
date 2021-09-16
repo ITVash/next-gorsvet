15e9e417320dfdad3668381019979e7939c34005
Vash <vash_dn@mail.ru>
2021-09-16 21:04:21 +0300

    Update PWA

--- client/next.config.js
+++ client/next.config.js
@@ -3,7 +3,7 @@ module.exports = withPWA({
 	pwa: {
 		dest: "public",
 		register: true,
-		skipWaiting: true,
+		//skipWaiting: true,
 	},
 	reactStrictMode: true,
 	images: {
@@ -23,22 +23,3 @@ module.exports = withPWA({
 			"https://api.telegram.org/bot832124158:AAEbvNYq9jRfJmLvQAaV4XOx2H3IQOM-ZNA/sendMessage",
 	},
 })
-/* module.exports = {
-	reactStrictMode: true,
-	images: {
-		domains: [
-			"http://localhost:3001",
-			"https://kkpdgs.cf",
-			"localhost:3001",
-			"kkpdgs.cf",
-			"localhost",
-		],
-	},
-	env: {
-		PRIVATE_KEY: "t5F_ECQJp4ZX-exWwhStyDVyfYi4qyh8Dwknt4R5ZU0",
-		API_URL: "http://localhost:3001/api/",
-		API_URLs: "https://kkpdgs.cf/api/",
-		API_TEL:
-			"https://api.telegram.org/bot832124158:AAEbvNYq9jRfJmLvQAaV4XOx2H3IQOM-ZNA/sendMessage",
-	},
-} */
--- client/pages/_document.tsx
+++ client/pages/_document.tsx
@@ -19,7 +19,41 @@ class MyDocument extends Document {
 				<Head>
 					<title>Донецкгорсвет</title>
 					<link rel='icon' href='/favicon.ico' />
+					<meta
+						name='viewport'
+						content='minimum-scale=1, initial-scale=1, width=device-width, shrink-to-fit=no, user-scalable=no, viewport-fit=cover'
+					/>
+					<meta charset='utf-8' />
+					<meta http-equiv='X-UA-Compatible' content='IE=edge' />
+					<meta name='application-name' content='Донецкгорсвет' />
+					<meta name='apple-mobile-web-app-capable' content='yes' />
+					<meta
+						name='apple-mobile-web-app-status-bar-style'
+						content='default'
+					/>
+					<meta name='apple-mobile-web-app-title' content='Донецкгорсвет' />
+					<meta name='description' content='Донецкгорсвет' />
+					<meta name='format-detection' content='telephone=no' />
+					<meta name='mobile-web-app-capable' content='yes' />
+
+					<meta name='msapplication-TileColor' content='#f69435' />
+					<meta name='msapplication-tap-highlight' content='no' />
+					<meta name='theme-color' content='#f69435' />
 					<link rel='manifest' href='/manifest.json' />
+					<link rel='shortcut icon' href='/favicon.ico' />
+					<link
+						rel='stylesheet'
+						href='https://fonts.googleapis.com/css?family=Roboto:300,400,500'
+					/>
+					<meta property='og:type' content='website' />
+					<meta property='og:title' content='Донецкгорсвет' />
+					<meta property='og:description' content='Донецкгорсвет' />
+					<meta property='og:site_name' content='Донецкгорсвет' />
+					<meta property='og:url' content='https://donetskgorsvet.tk' />
+					<meta
+						property='og:image'
+						content='https://yourdomain.com/icons/apple-touch-icon.png'
+					/>
 				</Head>
 				<body className={classNames("oh")}>
 					<Main />
